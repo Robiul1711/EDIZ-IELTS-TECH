76ad0836085d9dc0fde182183a5c30e27c4223bd
Robiul Islam Ashiq <ashiq.web.dev@gmail.com>
2026-01-05 14:44:59 +0600

    some changes

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById("root")).render(
     <ThemeProvider>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
-        <ReactQueryDevtools initialIsOpen={false} />
+        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
       </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>
